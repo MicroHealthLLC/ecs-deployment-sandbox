0ea34793299dd2f6cd027b7ba6a575080968efb4
jldroid19 <10508346+jldroid19@users.noreply.github.com>
2023-11-02 16:36:05 -0400

    test with new MH docker hub account

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World! ajshfkgjhsf;gfghdfgrthjfthhjh'
+    return 'Hello, World! ajshfkgjhsf;gh'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
